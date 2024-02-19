1e4f2db047063cf6fee5079f8648363358edf590
Jakub Rosner <jakub.rosner@logsystem.pl>
2024-02-19 09:26:23 +0100

    Refactor UsersService class

--- apps/server/src/app/users/users.service.ts
+++ apps/server/src/app/users/users.service.ts
@@ -1,51 +1,52 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User, UserTypeEnum } from './user.entity';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User, UserTypeEnum } from "./user.entity";
 
 @Injectable()
 export class UsersService {
 	private loggedUser: User = null;
 
-  constructor(
-	@InjectRepository(User)
-	private usersRepository: Repository<User>,
-  ) {}
+	constructor(
+		@InjectRepository(User)
+		private usersRepository: Repository<User>
+	) {}
 
-  findAll(): Promise<User[]> {
-	return this.usersRepository.find();
-  }
+	findAll(): Promise<User[]> {
+		return this.usersRepository.find();
+	}
 
-  findOne(id: number): Promise<User | null> {
-	return this.usersRepository.findOneBy({ id });
-  }
+	findOne(id: number): Promise<User | null> {
+		return this.usersRepository.findOneBy({ id });
+	}
 
 	async remove(id: number): Promise<void> {
 		await this.usersRepository.delete(id);
-  	}
+	}
 
-create(user: User): Promise<User> {
-	user.userType = UserTypeEnum.Candidate;
-	return this.usersRepository.save(user);
-}
+	create(user: User): Promise<User> {
+		user.userType = UserTypeEnum.Candidate;
+		return this.usersRepository.save(user);
+	}
 
-update(id: number, user: User): Promise<User | null> {
-	const updatedUser = { id, ...user };
-	return this.usersRepository.save(updatedUser);
-}
+	update(id: number, user: User): Promise<User | null> {
+		const updatedUser = { id, ...user };
+		return this.usersRepository.save(updatedUser);
+	}
 
-login(email: string, password: string): Promise<User> {
-	const user = this.usersRepository.findOne({ where: { email, password } });
+	login(email: string, password: string): Promise<User> {
+		const user = this.usersRepository.findOne({
+			where: { email, password }
+		});
 
-	user.then((onfulfilled) => {
-		this.loggedUser = onfulfilled
-	});
-	return user;
-}
+		user.then((onfulfilled) => {
+			this.loggedUser = onfulfilled;
+		});
+		return user;
+	}
 
-logout(): Promise<void> {
-	delete this.loggedUser;
-	return;
-}
-  
+	logout(): Promise<void> {
+		delete this.loggedUser;
+		return;
+	}
 }
